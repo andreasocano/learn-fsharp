62112b73cc6ac41a9325958eb732505e46c8f090
Andreas Suwanrat Ocano <andreas@rangholm.dk>
2021-03-15 21:56:39 +0100

    Init: ceasar cipher

---
+++ Ocano.LearnFSharp.ConsoleApp/Ciphers/CeasarCipher.fs
@@ -0,0 +1,28 @@
+﻿module CeasarCipher
+    let private alphabet = "abcdefghijklmnopqrstuvwxyz"
+    let private whitespaceIndex = -1
+    let private lengthOf input = String.length input
+    let private modulo n m =
+        let mm = if m > 0 then m else -m
+        let remainder = n % mm
+        if remainder < 0 then remainder + mm else remainder
+    let private getIndex letter =
+        if (letter = ' ') then whitespaceIndex
+        else alphabet.IndexOf(letter:char)
+    let private getLetter index =
+        if (index = whitespaceIndex) then ' '
+        else if (index < 0 && index > lengthOf alphabet) then ' '
+        else alphabet.[index]
+    let private getIndexAfterShift index shift =
+        if (index = whitespaceIndex) then whitespaceIndex
+        else modulo (index + shift) (lengthOf alphabet)
+    let encrypt message shift =
+        let messageLength = lengthOf message
+        let letters = List.map (fun i -> message.[i]) [0..messageLength - 1]
+        letters
+        |> List.map getIndex
+        |> List.map (fun c -> getIndexAfterShift c shift)
+        |> List.map getLetter
+        |> System.String.Concat
+    let decrypt message shift =
+        encrypt message -shift
---
+++ Ocano.LearnFSharp.ConsoleApp/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+[<EntryPoint>]
+let main args =
+    if args.Length <= 0
+    then
+        printfn "Please add number by which to shift the alphabet"
+    else
+        let shift = args.[0] |> int
+        let originalMessage = "attack at dawn"
+        printfn $"Original message: '{originalMessage}'"
+        let cipher = CeasarCipher.encrypt originalMessage shift
+        printfn $"Cipher: '{cipher}'"
+        let message = CeasarCipher.decrypt cipher shift
+        printfn $"Decrypted message: '{message}'"
+    0
